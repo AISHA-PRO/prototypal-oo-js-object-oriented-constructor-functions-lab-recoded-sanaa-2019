de3be197431aa2a3b8de5ef08314922c5ac7c5bc
Aisha Rashed <aisha1rashed@gmail.com>
2019-12-09 17:39:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function Scooter(year,color,model){
   this.model = model;
 }
 
-function Driver(year,color,model){
+function Driver(name,color,model){
   this.year = year;
   this.color = color;
   this.model = model;
